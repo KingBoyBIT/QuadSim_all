4cd1587d440e7da92e10a4afb1ddfc6b52b0a6ed
KingBoyBIT <405613185@qq.com>
2018-11-12 23:45:25 +0800

    testcode

--- temp.m
+++ temp.m
@@ -1,5 +1,5 @@
 clear,clc,close all
-syms r t
+syms r t b d
 
 x = r*cos(t);
 y = r*sin(t);
@@ -7,3 +7,9 @@ y = r*sin(t);
 diff(diff(x,t),t)
 diff(diff(y,t),t)
 
+tr=[b b b b;
+	0 -b 0 b;
+	-b 0 b 0;
+	-d d -d d];
+
+inv(tr)
